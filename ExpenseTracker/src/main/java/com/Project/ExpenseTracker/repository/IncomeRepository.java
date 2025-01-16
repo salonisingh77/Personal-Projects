8f7d7840f10f8898d015356ff8d293c334fca7c0
Saloni Singh <asaloni483@gmail.com>
2025-01-16 13:41:03 +0530

    Revert "adding Income enity"
    This reverts commit d06fe8e787a08081156659d94a19aa0e65639edb.

--- ExpenseTracker/src/main/java/com/Project/ExpenseTracker/repository/IncomeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Project.ExpenseTracker.repository;
-
-import com.Project.ExpenseTracker.entity.Income;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IncomeRepository extends JpaRepository<Income,Long> {
-}
